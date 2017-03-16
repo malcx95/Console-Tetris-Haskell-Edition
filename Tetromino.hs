3193f144546a792f6272e5f5ab8c1e1bccb2ca03
Malcolm Vigren <malvi108@student.liu.se>
2017-03-16 08:30:50 +0100

    Added bodies

--- Tetromino.hs
+++ Tetromino.hs
@@ -9,6 +9,90 @@ module Tetromino
 import Data.List
 import Control.Monad
 
+iBody =
+        [
+            ["eeeeeeee"]
+          , ["eeeeeeee"]
+          , ["IIIIIIII"]
+          , ["IIIIIIII"]
+          , ["eeeeeeee"]
+          , ["eeeeeeee"]
+          , ["eeeeeeee"]
+          , ["eeeeeeee"]
+        ]
+
+lBody =
+        [
+            ["eeeeeeee"]
+          , ["eeeeeeee"]
+          , ["LLLLLLee"]
+          , ["LLLLLLee"]
+          , ["LLeeeeee"]
+          , ["LLeeeeee"]
+          , ["eeeeeeee"]
+          , ["eeeeeeee"]
+        ]
+
+jBody =
+        [
+            ["eeeeeeee"]
+          , ["eeeeeeee"]
+          , ["eeJJJJJJ"]
+          , ["eeJJJJJJ"]
+          , ["eeeeeeJJ"]
+          , ["eeeeeeJJ"]
+          , ["eeeeeeee"]
+          , ["eeeeeeee"]
+        ]
+
+oBody =
+        [
+            ["eeeeeeee"]
+          , ["eeeeeeee"]
+          , ["eeOOOOee"]
+          , ["eeOOOOee"]
+          , ["eeOOOOee"]
+          , ["eeOOOOee"]
+          , ["eeeeeeee"]
+          , ["eeeeeeee"]
+        ]
+
+sBody =
+        [
+            ["eeeeeeee"]
+          , ["eeeeeeee"]
+          , ["eeeeSSSS"]
+          , ["eeeeSSSS"]
+          , ["eeSSSSee"]
+          , ["eeSSSSee"]
+          , ["eeeeeeee"]
+          , ["eeeeeeee"]
+        ]
+
+tBody =
+        [
+            ["eeeeeeee"]
+          , ["eeeeeeee"]
+          , ["eeTTTTTT"]
+          , ["eeTTTTTT"]
+          , ["eeeeTTee"]
+          , ["eeeeTTee"]
+          , ["eeeeeeee"]
+          , ["eeeeeeee"]
+        ]
+
+zBody =
+        [
+            ["eeeeeeee"]
+          , ["eeeeeeee"]
+          , ["ZZZZeeee"]
+          , ["ZZZZeeee"]
+          , ["eeZZZZee"]
+          , ["eeZZZZee"]
+          , ["eeeeeeee"]
+          , ["eeeeeeee"]
+        ]
+
 data SquareType 
     = SquareEmpty
     | SquareOut
@@ -38,6 +122,31 @@ data Tetromino =
               , x ::                Int
               , y ::                Int
               }
+
+charToSquare :: Char -> SquareType
+charToSquare c =
+    case c of
+        'e' -> SquareEmpty  
+        'X' -> SquareOut    
+        'I' -> SquareI      
+        'L' -> SquareL      
+        'J' -> SquareJ      
+        'O' -> SquareO      
+        'S' -> SquareS      
+        'T' -> SquareT      
+        'Z' -> SquareZ      
+
+bodyToSquares :: [[Char]] -> [[SquareType]]
+bodyToSquares (x:xs) =
+    let
+        row = map charToSquare (x !! 0)
+    in
+        row ++ bodyToSquares xs
+
+
+-- createTetromino :: Int -- random number between 0-6
+--                 -> Tetromino
+                
 
 squareToChar :: SquareType -> Char
 squareToChar x = case x of
